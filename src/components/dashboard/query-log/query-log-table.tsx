efa1e77eb71866fd98c2d5a042e495ebedea3dbb
PikuZheng <cba321123@gmail.com>
2026-05-30 08:37:52 +0800

    Fix query log domain filter mode

--- src/components/dashboard/query-log/query-log-table.tsx
+++ src/components/dashboard/query-log/query-log-table.tsx
@@ -268,7 +268,7 @@ function TableQueryLogs(): React.JSX.Element {
     useState<MRTColumnFilterFnsState>(
       () => Object.fromEntries(
         columns.map(({ accessorKey }) => {
-          return [accessorKey, 'equals'];
+          return [accessorKey, accessorKey === 'domain' ? 'contains' : 'equals'];
         })
       ) as MRTColumnFilterFnsState
     );
@@ -412,7 +412,7 @@ function TableQueryLogs(): React.JSX.Element {
 
         const filterMode = columnFilterFns[filter.id];
         if (filter.id === 'domain') {
-          const supportedModes = ['contains', 'equals', 'notEmpty', 'startwith', 'endwith'];
+          const supportedModes = ['contains', 'equals', 'notEmpty', 'startsWith', 'endsWith'];
           if (supportedModes.includes(filterMode)) {
             queryParam['domain_filter_mode'] = filterMode as string;
           }
@@ -637,7 +637,10 @@ function TableQueryLogs(): React.JSX.Element {
         children: errorMsg,
       }
       : undefined,
-    onColumnFilterFnsChange: setColumnFilterFns,
+    onColumnFilterFnsChange: (updaterOrValue) => {
+      pageCursor.current = null;
+      setColumnFilterFns(updaterOrValue);
+    },
     onColumnFiltersChange: (filters) => {
       pageCursor.current = null;
       setColumnFilters(filters);
